5dfa4597a03e6b791d68593f35b0d1ed95cd4ca3
hl2wic <hl2wic65@gmail.com>
2023-10-23 16:46:05 +0900

    first commit

---
+++ Demo SetTuple.py
@@ -0,0 +1,26 @@
+# Demo SetTuple.py
+a = {1,2,3,4}
+b = {3,4,4,5}
+print(a)
+print(b)
+print(a.union(b))
+print(a.intersection(b))
+print(a.difference)
+# print(a[0])
+
+
+print("---Tuple 형식 ----")
+tp = (1,2,3)
+print (tp)
+print (len(tp))
+print (tp[0])
+print("id : %s, name : %s" % ("kim","김유신"))
+
+# 함수 정의
+def calc(a,b):
+    return a+b, a*b
+
+# 함수 호출
+print(calc(3,4))
+
+
